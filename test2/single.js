8ab1ff74a072fd704b1a2a61582fa12750f5fabc
MitchellDouma <55207515+MitchellDouma@users.noreply.github.com>
2019-11-13 15:02:57 -0500

    Changed questions and put on one page

--- test2/single.js
+++ test2/single.js
@@ -3,43 +3,104 @@ Survey
     .StylesManager
     .applyTheme("default");
 
-var json = {
-    title: "American History",
-    pages: [{
-        questions: [
+    var json = {
+        title: "Employment Screening Questions",
+        pages: [
             {
-                type: "radiogroup",
-                name: "civilwar",
-                title: "When was the Civil War?",
-                choices: [
-                    "1750-1800", "1800-1850", "1850-1900", "1900-1950", "after 1950"
-                ],
-                correctAnswer: "1850-1900"
-            },
-            {
-                type: "radiogroup",
-                name: "libertyordeath",
-                title: "Who said 'Give me liberty or give me death?'",
-                choicesOrder: "random",
-                choices: [
-                    "John Hancock", "James Madison", "Patrick Henry", "Samuel Adams"
-                ],
-                correctAnswer: "Patrick Henry"
-            },
-            {
-                type: "radiogroup",
-                name: "magnacarta",
-                title: "What is the Magna Carta?",
-                choicesOrder: "random",
-                choices: [
-                    "The foundation of the British parliamentary system", "The Great Seal of the monarchs of England", "The French Declaration of the Rights of Man", "The charter signed by the Pilgrims on the Mayflower"
-                ],
-                correctAnswer: "The foundation of the British parliamentary system"
+                questions: [
+                    {
+                        type: "radiogroup",
+                        name: "reasonforleaving",
+                        title: "Why did the job candidate leave their previous job?",
+                        choices: [
+                            "Fired", "Laid-off", "School", "Found another opprotunity", "Other"
+                        ]
+                    },
+    
+                    {
+                        type: "radiogroup",
+                        name: "relationship",
+                        title: "How do you know the job candidate?",
+                        choices: [
+                            "Relative", "Former Employee", "Former co-worker", "Other"
+                        ]
+                    },
+
+                    {
+                        type: "radiogroup",
+                        name: "rehire",
+                        title: "If you had the chance to rehire the job candidate, would you?",
+                        choices: [
+                            "Yes", "No", "Depends on the situation"
+                        ]
+                    },
+     
+                    {
+                        type: "radiogroup",
+                        name: "strengths",
+                        title: "What were the job candadites greatest strengths?",
+                        choices: [
+                            "Communication", "Attitude", "Attentiveness", "Punctuality", "Hard-Working", "Other"
+                        ]
+                    },
+
+                    {
+                        type: "radiogroup",
+                        name: "weaknesses",
+                        title: "What were the job candadites greatest weaknesses?",
+                        choices: [
+                            "Communication", "Attitude", "Attentiveness", "Punctuality", "Hard-Working", "Other"
+                        ]
+                    },
+    
+                    {
+                        type: "radiogroup",
+                        name: "getalong",
+                        title: "Did the job candadite get along with their co-workers/management?",
+                        choices: [
+                            "Yes", "No", "Not with co-workers", "Not with management", "Sometimes"
+                        ]
+                    },
+
+                    {
+                        type: "radiogroup",
+                        name: "promotions",
+                        title: "Did the job candadite receive any promotions?",
+                        choices: [
+                            "Yes", "No"
+                        ]
+                    },
+    
+                    {
+                        type: "radiogroup",
+                        name: "accomplishments",
+                        title: "Did the job candadite have any major accomplishments during their time at the company?",
+                        choices: [
+                            "Yes", "No"
+                        ]
+                    },
+
+                    {
+                        type: "radiogroup",
+                        name: "conflict",
+                        title: "How did the job candadite handle conflict?",
+                        choices: [
+                            "Well", "Poorly", "Never had to"
+                        ]
+                    },
+    
+                    {
+                        type: "radiogroup",
+                        name: "teammember",
+                        title: "Was the job candadite more effective with a team or independently?",
+                        choices: [
+                            "Team", "Independent", "Was effective at both", "Was ineffective at both"
+                        ]
+                    }
+                ]
             }
         ]
-    }],
-    completedHtml: "<h4>You have answered correctly <b>{correctedAnswers}</b> questions from <b>{questionCount}</b>.</h4>"
-};
+    };
 
 window.survey = new Survey.Model(json);
 
